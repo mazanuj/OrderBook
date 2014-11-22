a4b9156987e05a0bc1e6f1024ec76bb676bbea9b
Yura Peleh <agronom.pele@gmail.com>
2014-11-22 21:36:12 +0200

    Added Updater.exe

--- OrderBook.Updater/MainForm.Designer.cs
+++ OrderBook.Updater/MainForm.Designer.cs
@@ -28,20 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lCurrent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lAviable = new System.Windows.Forms.Label();
+            this.prgrsBar = new System.Windows.Forms.ProgressBar();
+            this.bUpdate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.отчётToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пересоздатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущая версия: ";
+            // 
+            // lCurrent
+            // 
+            this.lCurrent.AutoSize = true;
+            this.lCurrent.Location = new System.Drawing.Point(138, 32);
+            this.lCurrent.Name = "lCurrent";
+            this.lCurrent.Size = new System.Drawing.Size(13, 13);
+            this.lCurrent.TabIndex = 1;
+            this.lCurrent.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Доступная версия: ";
+            // 
+            // lAviable
+            // 
+            this.lAviable.AutoSize = true;
+            this.lAviable.Location = new System.Drawing.Point(138, 59);
+            this.lAviable.Name = "lAviable";
+            this.lAviable.Size = new System.Drawing.Size(13, 13);
+            this.lAviable.TabIndex = 3;
+            this.lAviable.Text = "1";
+            // 
+            // prgrsBar
+            // 
+            this.prgrsBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.prgrsBar.Location = new System.Drawing.Point(15, 95);
+            this.prgrsBar.Name = "prgrsBar";
+            this.prgrsBar.Size = new System.Drawing.Size(457, 23);
+            this.prgrsBar.Step = 1;
+            this.prgrsBar.TabIndex = 4;
+            // 
+            // bUpdate
+            // 
+            this.bUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bUpdate.Enabled = false;
+            this.bUpdate.Location = new System.Drawing.Point(389, 27);
+            this.bUpdate.Name = "bUpdate";
+            this.bUpdate.Size = new System.Drawing.Size(83, 53);
+            this.bUpdate.TabIndex = 5;
+            this.bUpdate.Text = "Обновить";
+            this.bUpdate.UseVisualStyleBackColor = true;
+            this.bUpdate.Click += new System.EventHandler(this.bUpdate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tbStatus);
+            this.groupBox1.Location = new System.Drawing.Point(15, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 126);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Статус";
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.AcceptsReturn = true;
+            this.tbStatus.AcceptsTab = true;
+            this.tbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbStatus.Location = new System.Drawing.Point(7, 20);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.Size = new System.Drawing.Size(444, 100);
+            this.tbStatus.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отчётToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // отчётToolStripMenuItem
+            // 
+            this.отчётToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пересоздатьToolStripMenuItem});
+            this.отчётToolStripMenuItem.Name = "отчётToolStripMenuItem";
+            this.отчётToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.отчётToolStripMenuItem.Text = "Отчёт";
+            // 
+            // пересоздатьToolStripMenuItem
+            // 
+            this.пересоздатьToolStripMenuItem.Name = "пересоздатьToolStripMenuItem";
+            this.пересоздатьToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.пересоздатьToolStripMenuItem.Text = "Пересоздать отчёт";
+            this.пересоздатьToolStripMenuItem.Click += new System.EventHandler(this.GenerateStatusToolStripMenuItemClick);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bUpdate);
+            this.Controls.Add(this.prgrsBar);
+            this.Controls.Add(this.lAviable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lCurrent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "MainForm";
-            this.Text = "OrderBook Updater";
+            this.Text = "RealEstate обновление";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lCurrent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lAviable;
+        private System.Windows.Forms.ProgressBar prgrsBar;
+        private System.Windows.Forms.Button bUpdate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem отчётToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пересоздатьToolStripMenuItem;
     }
 }
 
